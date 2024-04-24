d09aed6eeff71011780e678fa4a5fb14efb0f10b
zmaholobela023 <zmaholobela023@student.wethinkcode.co.za>
2024-04-24 16:30:42 +0200

    zanny added a function

--- thebot/views.py
+++ thebot/views.py
@@ -9,19 +9,26 @@ from dotenv import load_dotenv
 import os
 
 ##implementing the DRY Method
-# def send_message(twilio_number,user_number,message):
-#     client.messages.create(
+def send_message(twilio_number,user_number,message):
+    account_sid = os.environ.get("ACCOUNT_SID")
+    auth_token = os.environ.get("AUTH_TOKEN")
+    client = Client(account_sid, auth_token)
 
-#     )
+    client.messages.create(
+        from_=user_number,
+        body=message,
+        to=twilio_number
+    )
+
 # # Create your views here.
 @csrf_exempt
 def home(request):
     load_dotenv()
-    account_sid = os.environ.get("ACCOUNT_SID")
-    auth_token = os.environ.get("AUTH_TOKEN")
+    # account_sid = os.environ.get("ACCOUNT_SID")
+    # auth_token = os.environ.get("AUTH_TOKEN")
     TWILID_PHONE_NUMBER = os.environ.get("TWILID_PHONE_NUMBER")
 
-    client = Client(account_sid, auth_token)
+    # client = Client(account_sid, auth_token)
 
     if request.method == "POST":
         message = request.POST
@@ -31,59 +38,38 @@ def home(request):
         the_Message = message["Body"]
 
         if the_Message.lower() in ['hey','hello','hi','hy','hei']:
-            client.messages.create(
-                from_= f'whatsapp:{TWILID_PHONE_NUMBER}',
-                body= f"Hey  {Name}",
-                to=the_number
-            )
+            send_message(f'whatsapp:{TWILID_PHONE_NUMBER}',the_number, f"Hey  {Name}")
         
         elif "quote" in the_Message.lower():
 
             category = the_Message[5: ].strip()
             user_quote = get_quote(category)
             if isinstance(user_quote,str):
-                client.messages.create(
-                    from_=TWILID_PHONE_NUMBER,
-                    body= user_quote,
-                    to= the_number
-                )
+                send_message(f'whatsapp:{TWILID_PHONE_NUMBER}',the_number, user_quote)
+                    
+              
             else:
                 output_quote = user_quote[0].get('quote')
                 quote_author = user_quote[0].get('author')
-                client.messages.create(
-                    from_= TWILID_PHONE_NUMBER,
-                    body = f"Quote: {output_quote} \n Authors: {quote_author}",
-                    to= the_number
-
-                )
-
+                send_message(f'whatsapp:{TWILID_PHONE_NUMBER}',the_number ,f"Quote: {output_quote} \n Authors: {quote_author}")
+              
         elif "joke" in the_Message.lower():
 
             limit = the_Message[5: ].strip()
             the_joke = get_joke(limit)
             
             if isinstance(the_joke,int):
-                client.messages.create(
-                    from_= TWILID_PHONE_NUMBER,
-                    body = the_joke,
-                    to = the_number
-                )
+                send_message(f'whatsapp:{TWILID_PHONE_NUMBER}',the_number,the_joke)
+              
+              
             else:
                 result_joke = the_joke[0].get('joke')
-                client.messages.create(
-                    from_ = TWILID_PHONE_NUMBER,
-                    body = f"The Joke: \n{result_joke}\n😂 😂 😂 😂 😂",
-                    to = the_number
-                )
-            
+                send_message(f'whatsapp:{TWILID_PHONE_NUMBER}',the_number,f"The Joke: \n{result_joke}\n😂 😂 😂 😂 😂")
+                 
         else:
-            client.messages.create(
-                from_=TWILID_PHONE_NUMBER,
-                body="Only trained to response to greetings in english to provide a quote",
-                to=the_number
-            )
-        
-        
+            send_message(f'whatsapp:{TWILID_PHONE_NUMBER}',the_number,"Only trained to response to greetings in english to provide a quote")
+            
+            
 
     return render(request, 'home.html')
 
